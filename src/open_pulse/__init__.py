3394d053d93bd39ddca4022333a05433a93ac91e
caviri <45425937+caviri@users.noreply.github.com>
2026-07-09 15:43:59 +0200

    chore: release 2.1.0

--- src/open_pulse/__init__.py
+++ src/open_pulse/__init__.py
@@ -3,4 +3,4 @@
 __all__ = ["__version__"]
 # Kept in sync with pyproject.toml by release-please (see the extra-files
 # entry in release-please-config.json). x-release-please-version
-__version__ = "2.0.0"  # x-release-please-version
+__version__ = "2.1.0"  # x-release-please-version
